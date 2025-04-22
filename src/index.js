c8132833f7c771663ccd4d7713be33a02ec9e66e
unknown <benjaminjesukorede@gmail.com>
2025-04-22 02:31:36 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename=" https://jesukorede.github.io/onboardingForm">
+    <BrowserRouter basename="/onboardingForm">
       <App />
     </BrowserRouter>
   </React.StrictMode>
